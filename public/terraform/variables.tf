a9e42200832e739c544d05ce3968aece2807fa3a
SooNa6214 <tlsalstjr469@gmail.com>
2026-06-08 17:24:31 +0900

    revert: variables.tf 기본값 원복 (tfvars에 이미 ARN 설정됨)

--- public/terraform/variables.tf
+++ public/terraform/variables.tf
@@ -237,7 +237,7 @@ variable "argocd_chart_version" {
 variable "additional_eks_admin_role_arns" {
   description = "Additional IAM role ARNs that should receive EKS cluster admin access"
   type        = list(string)
-  default     = ["arn:aws:iam::808379768010:role/kt_sgs_eks_admin_role"]
+  default     = []
 }
 
 variable "enable_site_to_site_vpn" {
