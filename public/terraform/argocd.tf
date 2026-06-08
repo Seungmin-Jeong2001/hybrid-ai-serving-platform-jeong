59ce8af36980bfd6a877aa7aa48c3cd7a814b737
kimbirdwon <pswk0907@gmail.com>
2026-06-08 09:49:38 +0900

    Argo CD 컴포넌트들이 system 노드 그룹에만 스케줄되도록 변경

--- public/terraform/argocd.tf
+++ public/terraform/argocd.tf
@@ -1,3 +1,5 @@
+# Terraform이 helm_release로 EKS 클러스터 안에 Argo CD를 설치하는 구조
+
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
@@ -15,6 +17,51 @@ resource "helm_release" "argocd" {
   create_namespace = false
   wait             = true
   timeout          = 900
+
+  values = [
+    yamlencode({
+      global = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+      controller = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+      dex = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+      redis = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+      repoServer = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+      server = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+      applicationSet = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+      notifications = {
+        nodeSelector = {
+          workload = "system"
+        }
+      }
+    })
+  ]
 
   depends_on = [kubernetes_namespace.argocd]
 }
